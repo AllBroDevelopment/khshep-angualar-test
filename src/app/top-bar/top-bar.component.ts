1b69fb8eba4f70631fbca17a90717da5dd016bc2
AllBroDevelopment <allbrodevelopment@gmail.com>
2019-12-27 22:18:43 -0500

    Added Menu Click Event Handlers

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -9,40 +9,39 @@ export class TopBarComponent implements OnInit {
 
   constructor() { }
 
+  // Toggle between showing and hiding the sidebar, and add overlay effect
+  openMenu($event) {
+    // Get the Sidebar
+    var mySidebar = document.getElementById("mySidebar");
+
+    // Get the DIV with overlay effect
+    var overlayBg = document.getElementById("myOverlay");
+
+    if (mySidebar.style.display === 'block') {
+      mySidebar.style.display = 'none';
+      overlayBg.style.display = "none";
+    } else {
+      mySidebar.style.display = 'block';
+      overlayBg.style.display = "block";
+    }
+  }
+
+  // Close the sidebar with the close button
+  closeMenu($event) {
+    // Get the Sidebar
+    var mySidebar = document.getElementById("mySidebar");
+
+    // Get the DIV with overlay effect
+    var overlayBg = document.getElementById("myOverlay");
+
+    mySidebar.style.display = "none";
+    overlayBg.style.display = "none";
+  }
+
   ngOnInit() {
   }
 
 }
-
-// Toggle between showing and hiding the sidebar, and add overlay effect
-function openMenu($event) {
-  // Get the Sidebar
-  var mySidebar = document.getElementById("mySidebar");
-
-  // Get the DIV with overlay effect
-  var overlayBg = document.getElementById("myOverlay");
-
-  if (mySidebar.style.display === 'block') {
-    mySidebar.style.display = 'none';
-    overlayBg.style.display = "none";
-  } else {
-    mySidebar.style.display = 'block';
-    overlayBg.style.display = "block";
-  }
-}
-
-// Close the sidebar with the close button
-function closeMenu($event) {
-  // Get the Sidebar
-  var mySidebar = document.getElementById("mySidebar");
-
-  // Get the DIV with overlay effect
-  var overlayBg = document.getElementById("myOverlay");
-
-  mySidebar.style.display = "none";
-  overlayBg.style.display = "none";
-}
-
 /*
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
